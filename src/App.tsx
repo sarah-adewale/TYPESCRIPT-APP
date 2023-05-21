2837d51c5dd32bab17c8fe2474edda12febbd9a6
Sarah Adewale <saraphynaadewale@gmail.com>
2023-05-21 21:10:13 +0100

    started typescript app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Store } from './pages/Store'
+
+function App() {
+  return (
+      <Container className='mb-4'> 
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/store' element={<Store/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+     </Container>
+  )
+  
+}
+
+export default App
